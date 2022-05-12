e19df1672561b2eb9bf57e358eebfa464fee2d9b
Paul Stone <pdjstone@users.noreply.github.com>
2022-05-12 18:33:19 +0100

    add file list function

--- emscripten/nspark-helper.js
+++ emscripten/nspark-helper.js
@@ -1,34 +1,43 @@
 
-let worker = new Worker('nspark-worker.js');
+let worker = null;
 let nextTid = 0;
 let archiveQueues = {};
 
 class AsyncQueue {
 
-	constructor(doFirst = null) {
-		this.buffer = [];
+  constructor(doFirst = null) {
+    this.buffer = [];
     this.resolve = null;
-    this.closed = false;
+    this.finished = false;
     this.doFirst = doFirst;
+    this.finishCallback = null;
   }
-  
+
+  onFinish(callback) {
+    this.finishCallback = callback;
+    return this;
+  }
+
   push(item) {
       if (this.resolve) {
-      	this.resolve(item);
+        this.resolve(item);
         this.resolve = null;
       } else {
-      	this.buffer.push(item);
+        this.buffer.push(item);
       }
   }
   
-  close() {
-    	this.closed = true;
+  close(finalData) {
+      this.finished = true;
       if (this.resolve)
-      	this.resolve();
+        this.resolve();
+      if (this.finishCallback) {
+        this.finishCallback(finalData);
+      }
   }
     
   [Symbol.asyncIterator]() {
-  	 let self = this;
+     let self = this;
      return {
         async next() { 
           if (self.doFirst) {
@@ -38,15 +47,15 @@ class AsyncQueue {
           let item = null;
           if (self.buffer.length > 0) {
             console.log('take from buffer');
-          	item = self.buffer.shift();
+            item = self.buffer.shift();
           } else { 
-          	item = await new Promise(resolve => {
-            	if (self.resolve)
-              	console.warning('resolve should be null');
-            	self.resolve = resolve;
-          	});
+            item = await new Promise(resolve => {
+              if (self.resolve)
+                console.warning('resolve should be null');
+              self.resolve = resolve;
+            });
           }
-          if (!self.closed) {
+          if (!self.finished) {
             return { done: false, value: item };
           } else {
             return { done: true };
@@ -57,20 +66,44 @@ class AsyncQueue {
 }
 
 
-worker.onmessage = function(msg) {
-    let tid = msg.data.tid;
-    if (!archiveQueues[tid]) {
-      console.error('no queue for tid ' + tid);
-    } else {
-      let q = archiveQueues[tid];
-      if (msg.data.hasOwnProperty('done')) {
-        q.close();
-      } else {
-        q.push(msg.data);
-      }
-    }
+function setupWorker(timeout) {
+  worker = new Worker('nspark-worker.js');
+  setTimeout(() => checkTerminate(timeout), timeout);
 }
 
+function checkTerminate(timeout) {
+  if (Object.keys(archiveQueues).length == 0) {
+    worker.terminate();
+    console.log("terminating nspark worker");
+    worker = null;
+  } else {
+    setTimeout(checkTerminate, timeout);
+  }
+}
+
+function getWorker(timeout = 5000) {
+  if (!worker)
+    setupWorker(timeout);
+  return worker;
+}
+
+export class NSpark {
+  constructor(buf) {
+    this.buf = buf;
+  }
+
+  static async fromUrl(url) {
+    let buf = await fetchArchiveUrl(url);
+    return new NSpark(buf);
+  }
+
+  list() {
+    return submitBufferToNsparkWorker(this.buf, 'list', buf => this.buf = buf);
+  }
+  unpack() {
+    return submitBufferToNsparkWorker(this.buf, 'unpack', buf => this.buf = buf);
+  }
+}
 
 async function fetchArchiveUrl(url) {
   let response = await fetch(url, {mode:'cors'});
@@ -78,15 +111,28 @@ async function fetchArchiveUrl(url) {
   return buf;
 }
 
-export function unarchiveUrl(url) {
-    console.log('unarchive url: ' + url);
-  
-    let q = new AsyncQueue(async () => {
-      let buf = await fetchArchiveUrl(url);
-      let tid = nextTid++;
-      archiveQueues[tid] = q;
-      worker.postMessage({'buf': buf, 'tid': tid}, [buf]);
-    });
-   
-    return q;
+function submitBufferToNsparkWorker(buf, action, bufReturnFn) {
+  let tid = nextTid++;
+  let q = new AsyncQueue().onFinish(bufReturnFn);
+  archiveQueues[tid] = q;
+  let worker = getWorker(300000);
+  worker.onmessage = function(msg) {
+    let tid = msg.data.tid;
+    if (!archiveQueues[tid]) {
+      console.error('no queue for tid ' + tid);
+    } else {
+      let q = archiveQueues[tid];
+      if (msg.data.hasOwnProperty('done')) {
+        q.close(msg.data.buf);
+        delete archiveQueues[tid];
+      } else {
+        q.push(msg.data.item);
+      }
+    }
+  }
+  worker.postMessage({
+    buf: buf, 
+    tid: tid, 
+    action: action}, [buf]);
+  return q;
 }
--- emscripten/nspark-worker.js
+++ emscripten/nspark-worker.js
@@ -6,32 +6,61 @@ let currentArchive = '';
 let currentDir = '';
 let completedFiles = [];
 let unpackQueue = [];
+let printErrHandler = null;
 
-createNSparkModule(
-    {
+//  filename filesize date filetype stored/compressed
+const LISTING_REGEX = /^([^\s]+)\s+(\d+)\s+(\d+-\w+-\d{4} \d\d:\d\d:\d\d)\s+&(\w+)\s+(\w+)/;
+
+function onCloseFile(path) {
+    if (path == currentArchive)
+        return;
+    if (completedFiles.length > 0) {
+        notifyFile(completedFiles.pop());
+    }
+    completedFiles.push(path);
+}
+
+function handlePrint(line) {
+    console.log(line);
+}
+
+function handlePrintErr(line) {
+    if (printErrHandler)
+        printErrHandler(line);
+    else
+        console.warn(line);
+}
+
+function capturePrintErr(actionFn, handlerFn) {
+    if (printErrHandler) {
+        console.warn("print err handler already set:", printErrHandler)
+    }
+    printErrHandler = handlerFn;
+    actionFn();
+    printErrHandler = null;   
+}
+
+async function init() {
+    nsparkModule = await createNSparkModule({
         noInitialRun:true, 
-    }).then(async function(Module) {
-    nsparkModule = Module;
-    Module.FS.trackingDelegate['onCloseFile'] = function(path) {
-        if (path == currentArchive)
-            return;
-        if (completedFiles.length > 0) {
-            notifyFile(completedFiles.pop());
-        }
-        completedFiles.push(path);
-      
-    } 
+        printErr: handlePrintErr,
+        print: handlePrint
+    });
+    nsparkModule.FS.trackingDelegate['onCloseFile'] = onCloseFile;
     doNext();
-});
+}
 
 function notifyFile(path) {
     let f = nsparkModule.FS.analyzePath(path);
     let buf = f.object.contents.buffer;
     self.postMessage({
         tid: currentTid, 
-        path: path.substr(currentDir.length+1), 
-        timestamp: f.object.timestamp, 
-        buf: buf}, [buf]);
+        item: {
+            path: path.substr(currentDir.length+1), 
+            timestamp: f.object.timestamp, 
+            buf: buf
+        }
+    }, [buf]);
     nsparkModule.FS.unlink(path);
 }
 
@@ -40,9 +69,11 @@ function doNext() {
         return;
     }
     while (unpackQueue.length > 0) {
+        let start = performance.now();
         let item = unpackQueue.shift();
         let tid = item.tid;
         let buf = item.buf;
+        let action = item.action;
 
         let filename = tid + '.arc';
         let unpackdir = '/unpack' + tid;
@@ -55,12 +86,39 @@ function doNext() {
         nsparkModule.FS.mkdir(unpackdir)
         nsparkModule.FS.createDataFile(unpackdir, filename, new Uint8Array(buf), true, true);
         nsparkModule.FS.chdir(unpackdir);
-        nsparkModule.callMain(['-u', '-T', filename]);
-        while (completedFiles.length > 0) {
-            notifyFile(completedFiles.pop());
+
+        if (action == 'unpack') {
+            nsparkModule.callMain(['-u', '-v', '-T', filename]);
+            while (completedFiles.length > 0) {
+                notifyFile(completedFiles.pop());
+            }
+        } else if (action == 'list') {
+            capturePrintErr(
+                () => nsparkModule.callMain(['-l', '-v', filename]),
+                line => {
+                    let match = line.match(LISTING_REGEX);
+                    // todo match listing for files with load/exec addresses 
+                    if (match) {
+                        let [all, filename, filesize, date, filetype, storage] = match;
+                        self.postMessage({
+                            tid: tid,
+                            item: {
+                                path: filename,
+                                filesize: parseInt(filesize),
+                                timestamp: Date.parse(date.replaceAll('-', ' ')),
+                                filetype: filetype.toLowerCase()
+                            }
+                        });
+                    } else {
+                        console.log("no match", line);
+                    }
+                });
+            
         }
         nsparkModule.FS.unlink(currentArchive);
-        self.postMessage({tid:tid, done:true});
+        self.postMessage({tid:tid, done:true, buf:buf}, [buf]);
+        let t = performance.now() - start;
+        console.log('worker took ' + t + 'ms');
     }
 }
 
@@ -68,3 +126,5 @@ self.onmessage = function(msg) {
     unpackQueue.push(msg.data);
     doNext();
 }
+
+init();
